b8dbc78b7609df488380be536b8e8b318bb305f6
fakeYan <18292025486@163.com>
2021-06-21 09:41:12 +0800

    提交文件

---
+++ cmake-build-debug/CMakeFiles/C___learning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___learning.dir/main.cpp.obj"
+  "C___learning.exe"
+  "C___learning.exe.manifest"
+  "C___learning.lib"
+  "C___learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
